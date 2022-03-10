1011706e90fa3085c1f9f89bfc5ae786f3502055
AnonymousAI <aege1.1.2002@gmail.com>
2022-03-10 10:19:33 -0800

    RandomGeneration
    Dungeon should randomly generate with assets for each room already made. The main room however get's filled because only the center sprite of the "closed wall" gets destroyed

---
+++ Endless Dungeon/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public int maxHp = 10; //Can be set to private later
+    public int hp = 10;
+    public int maxOil = 10; //Can be set to private later
+    public int oil = 10;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Endless Dungeon/Assets/_Scripts/RoomSpawner.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDir;
+    //1 --> need bottom door
+    //2 --> need top door
+    //3 --> need left door
+    //4 --> need right door
+
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+
+    public float waitTime = 1f;
+
+    void Start() {
+        Destroy(gameObject, waitTime);
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
+    }
+
+    void Spawn() {
+        if (spawned == false) {
+            if (openingDir == 1)
+            {
+                //need to spawn a room with a BOTTOM door
+                rand = Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+            }
+            else if (openingDir == 2)
+            {
+                //need to spawn a room with a TOP door
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+            }
+            else if (openingDir == 3)
+            {
+                //need to spawn a room with a LEFT door
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+            }
+            else if (openingDir == 4)
+            {
+                //need to spawn a room with a RIGHT door
+                rand = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+            }
+            spawned = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("SpawnPoint")) {
+            if (other.GetComponent<RoomSpawner>().spawned == false && spawned == false) {
+                //spawn walls blocking off any openings
+                Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+            spawned = true;
+        }
+    }
+}
